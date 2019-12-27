46bf99236d6175275b3292cba9e5f8bf0bf186e4
liu2010 <liu2010@qq.com>
2019-12-27 17:02:16 +0800

    b1 add c

--- src/main/java/T.java
+++ src/main/java/T.java
@@ -3,6 +3,11 @@ public class T {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println("b1");
-        System.out.println("b1 add");
+        System.out.println("b1 add  c");
+        c();
+    }
+
+    public static  void c(){
+        System.out.println(1);
     }
 }
